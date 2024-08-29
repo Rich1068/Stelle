f2b094d3c0b60c9e619180b0917d7cdc68b7ad5d
reykovlor <163377215+reykovlor@users.noreply.github.com>
2024-08-29 15:28:10 +0800

    Updated Profile UI for Blade

--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -1,31 +1,51 @@
 @extends('layouts.app')
+
 @section('body')
 
-<h1>Profile</h1>
+<div class="profile-container">
+    <div class="profile-header">
+        <div class="profile-picture">
+            @if($user->profile_picture == null)
+                <img src="{{ asset('storage/images/profile_pictures/default.jpg') }}" alt="Default profile picture"> 
+            @else 
+                <img src="{{ asset('storage/' . $user->profile_picture) }}" alt="Profile picture of {{ $user->first_name }}"> 
+            @endif
+        </div>
+        <div class="profile-info">
+            <h2 class="name-bold">{{ $user->first_name }} {{ $user->middle_name }} {{ $user->last_name }}</h2>
+            <p class="bold-blue">Super Admin</p>
+            <p>Tarlac, Bamban</p>
+        </div>
+        <a href="{{ route('profile.edit') }}" class="btn-edit">
+            <i class="fas fa-pencil-alt"></i> <span>Edit</span>
+        </a>
+    </div>
 
-<h2>
-    @if($user->profile_picture == null)
-        <img src="{{ asset('storage/images/profile_pictures/default.jpg') }}" alt="Profile pic" style="max-width: 200px; max-height: 100px;"> 
-    @else 
-        <img src="{{ asset($user->profile_picture) }}" alt="Profile pic" style="max-width: 200px; max-height: 100px;"> 
-    @endif {{ $user->first_name }} {{ $user->middle_name }} {{ $user->last_name }}
-</h2>
-<h3><b>About/Bio: </b><br> 
-@if ($user->description == null) 
-    N/A 
-@else 
-    {{ $user->description }} 
-@endif </h3><br><br>
-<h3>
-<b>Information</b> <br> 
-Email: {{ $user->email }} <br> 
-Contact Number: @if ($user->contact_number == null) N/A @else {{ $user->contact_number }} @endif 
-<br> Country: @if($user->country_id == null) N/A @else {{ $countryTable->countryname }} <img src="{{ asset("storage/images/flags/{$countryTable->code}.png") }}" alt="Profile pic" style="max-width: 50px; max-height: 50px;"> @endif
-<br> Gender: @if($user->gender == null) N/A @else {{ $user->gender }} @endif </h3>
+    <div class="profile-body">
+        <div class="about-section">
+            <h3 class="bold-blue">
+                <i class="fas fa-user-circle"></i> About/Bio
+            </h3>
+            <p>@if($user->description == null) N/A @else {{ $user->description }} @endif</p>
+        </div>
 
+        <div class="info-section">
+            <h3 class="bold-blue">
+                <i class="fas fa-info-circle"></i> Information
+            </h3>
+            <p><strong>Email:</strong> {{ $user->email }}</p>
+            <p><strong>Contact Number:</strong> @if($user->contact_number == null) N/A @else {{ $user->contact_number }} @endif</p>
+            <p><strong>Country:</strong> 
+                @if($user->country_id == null) 
+                    N/A 
+                @else 
+                    {{ $countryTable->countryname }} 
+                    <img src="{{ asset('storage/images/flags/' . $countryTable->code . '.png') }}" alt="Flag of {{ $countryTable->countryname }}" class="flag-icon"> 
+                @endif
+            </p>
+            <p><strong>Gender:</strong> @if($user->gender == null) N/A @else {{ $user->gender }} @endif</p>
+        </div>
+    </div>
+</div>
 
-
-<a href="{{route('profile.edit')}}" class="btn btn-primary">
-    <span>Edit</span>
-</a>
 @endsection
