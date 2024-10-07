55e4d3b08e06d8b8f43b1bd3270c5b43f4ef0599
kovlor13 <127574325+kovlor13@users.noreply.github.com>
2024-10-07 19:48:53 +0800

    Minor Fix

--- resources/views/emails/verify-email.blade.php
+++ resources/views/emails/verify-email.blade.php
@@ -7,8 +7,8 @@
 </head>
 <body style="margin: 0; padding: 0; background-color: #F8C8C8;">
 
-    <table width="100%" cellpadding="0" cellspacing="0" border="0" style="background-color: #B8E1F9; padding: 50px 0;">
-        <tr>
+    <table width="100%" cellpadding="0" cellspacing="0" border="0" style="background: linear-gradient(to bottom right, #F99C9C, #ACDFF6) !important;">
+        <tr>    
             <td align="center">
                 <table width="450" cellpadding="0" cellspacing="0" border="0" style="background-color: #ffffff; border-radius: 10px; box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);">
                     <tr>
