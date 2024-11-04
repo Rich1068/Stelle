5056174b4c336955236902056de78a8fe034ac00
Rich1068 <rs106848@gmail.com>
2024-11-04 21:16:43 +0800

    small fix on time

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -311,7 +311,7 @@ class EventController extends Controller
             'mode' => ['required', 'string', 'in:onsite,virtual'],
             'address' => ['required', 'string', 'max:255'],
             'start_time' => ['required', 'date_format:H:i'],
-            'end_time' => ['required', 'date_format:H:i'],
+            'end_time' => ['required', 'date_format:H:i', 'different:start_time'],
             'capacity' => ['required', 'integer', 'min:1'],
             'event_banner' => ['nullable', 'image', 'mimes:jpeg,png,jpg,gif,svg', 'max:2048']
         ]);
